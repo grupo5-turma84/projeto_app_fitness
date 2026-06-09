bd81d6f1c09ba19bf41651b29dd60aaaa15ca55d
kaua alves <kauaalves7163@icloud.com>
2026-06-09 14:51:56 -0300

    first commit

---
+++ src/main/java/com/generation/fitness/ProjetoAppFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.generation.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAppFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAppFitnessApplication.class, args);
+	}
+
+}
