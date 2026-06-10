3453d49801573e0e85362fd21c6f51e0fed46235
Fernando <fernandogarcia98@hotmail.com>
2026-06-10 10:26:45 -0300

    Implementação Swagger

--- src/main/java/com/generation/fitness/model/Usuario.java
+++ src/main/java/com/generation/fitness/model/Usuario.java
@@ -13,48 +13,80 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank(message = "O atributo nome é obrigatório!")
-    private String nome;
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	private String nome;
 
-    @NotBlank(message = "O atributo usuário (e-mail) é obrigatório!")
-    @Email(message = "O atributo usuário deve ser um e-mail válido!")
-    private String usuario;
+	@Schema(example = "email@email.com.br")
+	@NotBlank(message = "O atributo usuário (e-mail) é obrigatório!")
+	@Email(message = "O atributo usuário deve ser um e-mail válido!")
+	private String usuario;
 
-    @NotBlank(message = "A senha é obrigatória!")
-    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
-    private String senha;
+	@NotBlank(message = "A senha é obrigatória!")
+	@Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+	private String senha;
 
-    private String foto;
+	private String foto;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
-    @JsonIgnoreProperties("usuario")
-    private List<Exercicio> exercicio;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Exercicio> exercicio;
 
-    // Getters e Setters manuais
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+	// Getters e Setters manuais
+	public Long getId() {
+		return id;
+	}
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getUsuario() { return usuario; }
-    public void setUsuario(String usuario) { this.usuario = usuario; }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getSenha() { return senha; }
-    public void setSenha(String senha) { this.senha = senha; }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getFoto() { return foto; }
-    public void setFoto(String foto) { this.foto = foto; }
+	public String getUsuario() {
+		return usuario;
+	}
 
-    public List<Exercicio> getExercicio() { return exercicio; }
-    public void setExercicio(List<Exercicio> exercicio) { this.exercicio = exercicio; }
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Exercicio> getExercicio() {
+		return exercicio;
+	}
+
+	public void setExercicio(List<Exercicio> exercicio) {
+		this.exercicio = exercicio;
+	}
 }
